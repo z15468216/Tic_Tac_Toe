84ba1aa4b4b0d9805ddd478d1bcc11f3f919cd2e
z15468216 <wolfling15468216@gmail.com>
2021-01-19 16:47:52 +0800

    typesetting

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.tic_tac_toe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
